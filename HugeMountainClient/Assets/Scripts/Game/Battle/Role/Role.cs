95aa7395498bf2b9a7b542e715a61f2f0c267541
fuchao <410662979@qq.com>
2019-05-09 23:57:56 +0800

    增加spine库 挑选一个测试sample

--- HugeMountainClient/Assets/Scripts/Game/Battle/Role/Role.cs
+++ HugeMountainClient/Assets/Scripts/Game/Battle/Role/Role.cs
@@ -4,7 +4,13 @@ using System.Collections;
 public class Role : MonoBehaviour {
     public int hp = 0;
     public int mp = 0;
-    public float speed = 0;
+    public float speed = 0;   //移动速率
+    public float attSpeed = 0;//攻击频率
+    public float missRate = 0;//闪避
+    public float powerAttRate = 0;//暴击概率
+    
+
+    
 
     void Start() {
 
@@ -13,4 +19,24 @@ public class Role : MonoBehaviour {
     void Update() {
 
     }
+
+    void att(){
+
+    }
+
+    void hit()
+    {
+
+    }
+
+    void move()
+    {
+
+    }
+
+    void dead()
+    {
+
+    }
+
 }
