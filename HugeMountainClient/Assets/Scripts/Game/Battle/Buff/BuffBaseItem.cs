4bf7b5a5cde75a2eef25e2fe367b9523b8f05a2b
许卡拉 <308974268@qq.com>
2019-05-18 23:51:07 +0800

    [*] 加入一个大致的BUFF系统

---
+++ HugeMountainClient/Assets/Scripts/Game/Battle/Buff/BuffBaseItem.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ *          id: buffId,
+            name: row.name,
+            desc: row.desc,
+            probability: row.probability,
+            maxCount: row.max_count,
+            kind: row.kind,
+            disperse: row.disperse,
+            delay: row.delay,
+            duration: row.duration,
+            buffType: row.buff_type,
+            isWaveClear: row.buff_is_clear === 1,
+            addTimestamp: 0,
+            startTimestamp: 0,
+            endTimestamp: 0,
+            state: BUFF_STATE_CONFIG.DEFAULT,
+            data: {}
+*/
+
+public enum BuffState {
+    None = 0,
+    Run = 1,
+    Wait = 2,
+    End = 3,
+}
+
+public class BuffBaseItem {
+    public int id; //BUFF ID
+    public float timer; //计时器
+    public float duration; //持续时间
+    public int maxCount; //最大数量
+    public float delay; //延迟时间
+    public float addTime; //添加时间
+    public float startTime; //开始时间
+    public float endTime; //结束时间
+    // config
+    public string buffType; //BUFF类型
+    public BuffState buffState; //BUFF状态
+
+    virtual public void init(Role attacker) {
+
+    }
+
+    virtual public void onBegin() {
+
+    }
+
+    virtual public void onEnd() {
+
+    }
+
+    virtual public void onAttack(Role emitData) {
+
+    }
+
+    virtual public void onAttackHit(Role emitData) {
+
+    }
+
+    virtual public void onHurt(Role emitData) {
+
+    }
+
+    virtual public void onDodge(Role emitData) {
+
+    }
+
+    virtual public void onHit(Role emitData) {
+
+    }
+
+    virtual public void onSkill(Role emitData) {
+
+    }
+
+    virtual public void clear() {
+        startTime = 0;
+        addTime = 0;
+        endTime = 0;
+        duration = 0;
+    }
+
+    virtual public void onUndefine(BuffEmitType type, Role emitData) {
+
+    }
+
+    public void update() {
+        timer += Time.fixedTime;
+    }
+
+    virtual public void dispose(BuffEmitType type, Role emitData) {
+        switch (type) {
+            case BuffEmitType.Attack:
+                onAttack(emitData);
+                break;
+            case BuffEmitType.AttackHit:
+                onAttackHit(emitData);
+                break;
+            case BuffEmitType.Dodge:
+                onDodge(emitData);
+                break;
+            case BuffEmitType.Hit:
+                onHit(emitData);
+                break;
+            case BuffEmitType.Skill:
+                onSkill(emitData);
+                break;
+            case BuffEmitType.Hurt:
+                onHurt(emitData);
+                break;
+            default:
+                onUndefine(type, emitData);
+                break;
+        }
+    }
+}
--- HugeMountainClient/Assets/Scripts/Game/Battle/Buff/BuffMgr.cs
+++ HugeMountainClient/Assets/Scripts/Game/Battle/Buff/BuffMgr.cs
@@ -2,14 +2,145 @@
 using System.Collections;
 using System.Collections.Generic;
 
+public enum BuffEmitType {
+    None = 0,
+    Attack = 1,
+    AttackHit = 2,
+    Hit = 3,
+    Skill = 4,
+    Dodge = 5,
+    Hurt = 6,
+}
+
 public class BuffMgr {
-    private Dictionary<string, List<BuffItemVO>> _mapBuff = new Dictionary<string, List<BuffItemVO>>();
+    private Dictionary<string, List<BuffBaseItem>> _mapBuff = new Dictionary<string, List<BuffBaseItem>>();
+    private float _nowTime;
 
-    public void initBuff() {
+    public void init() {
+        _nowTime = 0;
+    }
 
+    public void clear() {
+        foreach (KeyValuePair<string, List<BuffBaseItem>> kv in _mapBuff) {
+            if (kv.Value == null) {
+                continue;
+            }
+
+            if (kv.Value.Count <= 0) {
+                continue;
+            }
+
+            kv.Value.RemoveAll((obj) => obj.buffState == BuffState.End);
+
+            foreach (var buff in kv.Value) {
+                endBuff(buff);
+                buff.clear();
+            }
+
+            kv.Value.Clear();
+        }
+
+        _mapBuff.Clear();
     }
 
     public void update() {
+        _nowTime += Time.fixedTime;
 
+        foreach (KeyValuePair<string, List<BuffBaseItem>> kv in _mapBuff) {
+            if (kv.Value == null) {
+                continue;
+            }
+
+            if (kv.Value.Count <= 0) {
+                continue;
+            }
+
+            kv.Value.RemoveAll((obj) => obj.buffState == BuffState.End);
+
+            foreach (var buff in kv.Value) {
+                if (buff.buffState == BuffState.Run) {
+                    if (buff.duration >= 0 && _nowTime > buff.startTime + buff.duration) {
+                        endBuff(buff);
+                    } else {
+                        buff.update();
+                    }
+                } else if (buff.buffState == BuffState.Wait) {
+                    if (_nowTime > buff.addTime + buff.delay) {
+                        beginBuff(buff);
+                    }
+                }
+            }
+        }
+    }
+
+    private void endBuff(BuffBaseItem buff) {
+        if (buff.buffState == BuffState.Run) {
+            buff.endTime = _nowTime;
+            buff.buffState = BuffState.End;
+            buff.onEnd();
+        }
+    }
+
+    private void beginBuff(BuffBaseItem buff) {
+        if (buff.buffState == BuffState.Wait) {
+            buff.startTime = _nowTime;
+            buff.buffState = BuffState.Run;
+            buff.onBegin();
+        }
+    }
+
+    public bool addBuff(string buffId) {
+        var buff = new BuffBaseItem();
+        if (!canAddBuff(buff)) {
+            return false;
+        }
+        initBuff(buff);
+        return true;
+    }
+
+    private void initBuff(BuffBaseItem buff) {
+        if (buff.buffState == BuffState.None) {
+            buff.addTime = _nowTime;
+            buff.buffState = BuffState.Wait;
+
+            if (!_mapBuff.ContainsKey(buff.buffType)) {
+                _mapBuff[buff.buffType] = new List<BuffBaseItem>();
+            }
+
+            _mapBuff[buff.buffType].Add(buff);
+
+            if (buff.maxCount > -1) {
+                var findList = _mapBuff[buff.buffType].FindAll((obj) => obj.id == buff.id);
+                if (findList.Count > buff.maxCount) {
+                    findList.Sort((a, b) => (int)(a.addTime - b.addTime));
+                    int removeCount = findList.Count - buff.maxCount;
+                    for (var i = 0; i < removeCount; ++i) {
+                        endBuff(findList[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    private bool canAddBuff(BuffBaseItem buff) {
+        return true;
+    }
+
+    public void emitBuff(BuffEmitType type, Role emitData) {
+        foreach (KeyValuePair<string, List<BuffBaseItem>> kv in _mapBuff) {
+            if (kv.Value == null) {
+                continue;
+            }
+
+            if (kv.Value.Count <= 0) {
+                continue;
+            }
+
+            foreach (var buff in kv.Value) {
+                if (buff.buffState == BuffState.Run) {
+                    buff.dispose(type, emitData);
+                }
+            }
+        }
     }
 }
